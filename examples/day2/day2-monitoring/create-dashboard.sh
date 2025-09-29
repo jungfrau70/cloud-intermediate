b0ab7f0b27d945bf2a08044ca9295bac18ea6177
jungfrau70 <inhwan.jung@gmail.com>
2025-09-30 08:19:34 +0900

    Update: 2025-09-30 08:19:34 - archive/lecture-test-results.log and 6630 more files

--- examples/day2/day2-monitoring/create-dashboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# CloudWatch 대시보드 생성
-aws cloudwatch put-dashboard \
-  --dashboard-name "MyApp Dashboard" \
-  --dashboard-body file://cloudwatch-dashboard.json
-
-# 대시보드 확인
-aws cloudwatch get-dashboard --dashboard-name "MyApp Dashboard"
--- examples/day2/day2-monitoring/create-gcp-dashboard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Cloud Monitoring 대시보드 생성
-gcloud monitoring dashboards create \
-  --config-from-file=gcp-dashboard.json
-
-# 대시보드 목록 확인
-gcloud monitoring dashboards list
